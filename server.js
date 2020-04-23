dba07087ec2ed43484ea681d600009d59d22e760
sujithvn <mail.sujithvn@gmail.com>
2020-04-23 21:26:16 +0530

    MongoDB Mongoose setup

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 dotenv.config();
 
 // Initialise
 const app = express();
 const PORT = process.env.SERVER_PORT || 8000;
+const DATABASE = process.env.DATABASE;
 
+//DB connectivity
+mongoose.connect(DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("Database connected"));
+  
 
 // Middleware
 app.use(morgan("dev"));
