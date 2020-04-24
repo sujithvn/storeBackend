98d59d4e2afa559956269e9b368123a7b557de79
sujithvn <mail.sujithvn@gmail.com>
2020-04-24 19:47:08 +0530

    Validate existing email - Legacy API

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.userRegisterValidator = (req, res, next) => {
     req.check("fname", "First Name is required field").notEmpty();
     req.check("lname", "Last Name is required field").notEmpty();
@@ -5,6 +7,15 @@ exports.userRegisterValidator = (req, res, next) => {
         .isEmail()
         .withMessage("Invalid or empty eMail")
         .normalizeEmail();
+    req.check("email", "eMail already registered")
+        .custom(value => {
+          return User.findOne({ email: value })
+            .then(userDB => {
+              if (userDB)  {
+                return Promise.reject();
+              }
+            })
+          });
     req.check("password", "Password is required field").notEmpty();
     req.check("password")
       .isLength({ min: 6 })
@@ -12,11 +23,21 @@ exports.userRegisterValidator = (req, res, next) => {
       .matches(/\d/)
       .withMessage("Password must contain atleast one digit");
   
-    const errors = req.validationErrors();
-    if (errors) {
-      const firsterror = errors.map(error => error.msg)[0];
-      return res.status(400).json({ error: firsterror });
-    }
-    next();
+    // const errors = req.validationErrors();
+    // if (errors) {
+      const errors = req.asyncValidationErrors()
+      .then(msgs => {
+        if (!msgs) {
+          return next();
+        } else {
+          console.log('Got validation errors... processing');
+          console.log(msgs);
+        }
+      })
+      .catch(errors => {
+        const firsterror = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firsterror });
+      })
+    //next();
   };
   
